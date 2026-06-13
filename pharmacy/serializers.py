eac0861fa7a115f58454fbd4ded16afee825ad44
MobadruCompany <tallbadru@gmail.com>
2026-06-14 01:52:09 +0300

    solve error

--- pharmacy/serializers.py
+++ pharmacy/serializers.py
@@ -56,21 +56,30 @@ class StockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = '__all__'
+        read_only_fields = ['pharmacy']
+
+    def create(self, validated_data):
+
+        request = self.context['request']
+
+        pharmacy = request.user.staff_profile.pharmacy
+
+        validated_data['pharmacy'] = pharmacy
+
+        return Stock.objects.create(**validated_data)
 
 
 # =========================
 # RESERVATION (CORE SYSTEM)
 # =========================
 class ReservationSerializer(serializers.ModelSerializer):
-
-    # readable fields for frontend
-    user_name = serializers.ReadOnlyField(source='user.username')
-    pharmacy_name = serializers.ReadOnlyField(source='pharmacy.name')
-    medicine_name = serializers.ReadOnlyField(source='medicine.name')
+    patient_name = serializers.CharField(source="user.username", read_only=True)
+    medicine_name = serializers.CharField(source="medicine.name", read_only=True)
+    pharmacy_name = serializers.CharField(source="pharmacy.name", read_only=True)
 
     class Meta:
         model = Reservation
-        fields = '__all__'
+        fields = "__all__"
 
     # SECURITY FIX: prevent frontend from faking user
     def create(self, validated_data):
